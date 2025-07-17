cc359a12481a1f9dce3e7c4201e7c2397ee98da4
Cyril-Grelier <cyril.grelier@outlook.fr>
2025-07-17 10:29:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Example of call to ma_lib
+
+Simple project to call ma_lib (https://github.com/Cyril-Grelier/ma_lib_cpp).
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <ma_lib.h>
+
+int main() {
+    int a = 5;
+    int b = 7;
+    int result = add(a, b);
+    std::cout << "Le résultat de l'addition est : " << result << std::endl;
+    return 0;
+}
